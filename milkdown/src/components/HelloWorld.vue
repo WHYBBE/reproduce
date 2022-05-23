dfe363ea51913f9564295e909be353baab6f8e76
HY Wang <Hex574859@outlook.com>
2022-05-23 16:44:11 +0800

    milkdown bug

---
+++ milkdown/src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div ref="editor"></div>
+</template>
+
+<script setup lang="ts">
+  import { defaultValueCtx, Editor, rootCtx } from "@milkdown/core";
+  import { nord } from "@milkdown/theme-nord";
+  import { commonmark } from "@milkdown/preset-commonmark";
+  import { onMounted, ref } from "vue";
+
+  const props = defineProps({
+      msg: { type: String, default: "" },
+  });
+
+  const editor = ref();
+
+  onMounted(() => {
+    Editor.make()
+      .config((ctx) => {
+          ctx.set(rootCtx, editor.value);
+          ctx.set(defaultValueCtx, props.msg);
+      })
+      .use(nord)
+      .use(commonmark)
+      .create();
+  });
+</script>
+
+<style></style>
